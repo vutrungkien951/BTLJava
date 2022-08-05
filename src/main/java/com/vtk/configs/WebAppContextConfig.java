7b99bce8791cc3121fc5d8dd9b103f5e200c4ca9
vutrungkien <vutrungkien951@gmail.com>
2022-08-05 21:38:53 +0700

    Add existing project files to Git

---
+++ src/main/java/com/vtk/configs/WebAppContextConfig.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.vtk.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+/**
+ *
+ * @author kien
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {
+    "com.vtk.controllers"
+})
+public class WebAppContextConfig implements WebMvcConfigurer{
+    
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer conf){
+        conf.enable();
+    }
+    
+    @Bean
+    public InternalResourceViewResolver getInternalResourceViewResolver (){
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setViewClass(JstlView.class);
+        resolver.setPrefix("/WEB-INF/jsp/");
+        resolver.setSuffix(".jsp");
+        
+        return resolver;
+    }
+}
